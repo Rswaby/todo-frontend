ca95de7aacf3d23c7537a1368a99e4beb51c9864
Rohan Swaby <rohan_swaby51@yahoo.com>
2019-08-22 16:03:22 -0400

    Navbar fix

--- src/components/Navigation/index.js
+++ src/components/Navigation/index.js
@@ -1,54 +1,57 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
-
 import { AuthUserContext } from '../Session';
 import SignOutButton from '../SignOut';
 import * as ROUTES from '../../constants/routes';
 import * as ROLES from '../../constants/roles';
 
+const style = {
+  link: {
+    "text-decoration": "none",
+    "color": "black",
+    "text-align": "center",
+  }
+}
 const Navigation = () => (
   <AuthUserContext.Consumer>
     {authUser =>
       authUser ? (
         <NavigationAuth authUser={authUser} />
       ) : (
-        <NavigationNonAuth />
-      )
+          <NavigationNonAuth />
+        )
     }
   </AuthUserContext.Consumer>
 );
 
 const NavigationAuth = ({ authUser }) => (
-  <ul>
-    <li>
-      <Link to={ROUTES.LANDING}>Landing</Link>
-    </li>
-    <li>
-      <Link to={ROUTES.HOME}>Home</Link>
-    </li>
-    <li>
-      <Link to={ROUTES.ACCOUNT}>Account</Link>
-    </li>
+  <div id={"nav-bar"} className={"nav justify-content-end"}>
+    <div id={"nav-bar-item"}>
+      <Link style={style.link} to={ROUTES.HOME}>HOME</Link>
+    </div>
+    <div id={"nav-bar-item"}>
+      <Link style={style.link} to={ROUTES.ACCOUNT}>ACCOUNT</Link>
+    </div>
     {!!authUser.roles[ROLES.ADMIN] && (
-      <li>
-        <Link to={ROUTES.ADMIN}>Admin</Link>
-      </li>
+      <div id={"nav-bar-item"}>
+        <Link style={style.link} to={ROUTES.ADMIN}>ADMIN</Link>
+      </div>
     )}
-    <li>
+    <div id={"nav-bar-item"}>
       <SignOutButton />
-    </li>
-  </ul>
+    </div>
+  </div>
 );
 
 const NavigationNonAuth = () => (
-  <ul>
-    <li>
-      <Link to={ROUTES.LANDING}>Landing</Link>
-    </li>
-    <li>
-      <Link to={ROUTES.SIGN_IN}>Sign In</Link>
-    </li>
-  </ul>
+  <nav id={"nav-bar"} className={"navbar navbar-light bg-light justify-content-end"}>
+    <div id={"nav-bar-item"} className={"nav-item"}>
+      <Link style={style.link} to={ROUTES.LANDING}>LANDING</Link>
+    </div>
+    <div id={"nav-bar-item"} className={"nav-item"}>
+      <Link style={style.link} to={ROUTES.SIGN_IN}>SIGN IN</Link>
+    </div>
+  </nav>
 );
 
 export default Navigation;
